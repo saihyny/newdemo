8909e06386ac5b514c9901ef97a8a17a465de7d5
saihyny <satyasaikiranrocks@gmail.com>
2023-07-08 12:27:03 +0530

    commit

--- html.js
+++ html.js
@@ -1,6 +1,8 @@
-var item = document.getElementsByClassName("list-group-item")
-item[2].style.backgroundColor = "green" ;
-for(var i=0;i<item.length;i++)
-{
-    item[i].style.fontWeight = 'bold';
-}
+// var item = document.getElementsByClassName("list-group-item")
+// item[2].style.backgroundColor = "green" ;
+// for(var i=0;i<item.length;i++)
+// {
+//     item[i].style.fontWeight = 'bold';
+// }
+var li=document.getElementsByTagName("li");
+li[4].style.backgroundColor="green";
