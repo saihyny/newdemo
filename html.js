e937ed8e14ef7f69f8249e6313166eae1a15fe3c
saihyny <satyasaikiranrocks@gmail.com>
2023-07-08 16:02:07 +0530

    sharpner code

--- html.js
+++ html.js
@@ -4,5 +4,19 @@
 // {
 //     item[i].style.fontWeight = 'bold';
 // }
-var li=document.getElementsByTagName("li");
-li[4].style.backgroundColor="green";
+// var li=document.getElementsByTagName("li");
+// li[4].style.backgroundColor="green";
+// var seconditem = document.querySelector(".list-group-item:nth-child(2)");
+// seconditem.style.backgroundColor="green"
+var thirditem = document.querySelector(".list-group-item:nth-child(3)");
+thirditem.style.color = 'white';
+var secitem = document.querySelectorAll(".list-group-item:nth-child(2)");
+for(var i=0;i<secitem.length;i++)
+{
+    secitem[i].style.color="green"
+}
+var odd = document.querySelectorAll(".list-group-item:nth-child(odd)");
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor="green"
+}
