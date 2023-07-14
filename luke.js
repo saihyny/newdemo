0bfdfd0cc061146f495d7a812b0f78608bf6f002
saihyny <satyasaikiranrocks@gmail.com>
2023-07-14 20:59:41 +0530

    sharpner

--- luke.js
+++ luke.js
@@ -1,10 +1,25 @@
-var newobj = 
-{
-    name:"daniel", 
-    age:22
-}
-var convertString = JSON.stringify(newobj);
+var itemList = document.getElementById('list-group');
+var form = document.getElementById('addForm');
 
-localStorage.setItem("newobj",convertString);
-var getitem= localStorage.getItem("newobj");
-console.log(getitem)
+form.addEventListener("submit", addItem);
+
+function addItem(e) {
+    e.preventDefault();
+
+    var name2 = document.getElementById('username').value;
+    var number = document.getElementById('mobile').value;
+    var email = document.getElementById('email').value;
+    var mergedText = name2 + "-" + number + "-" + email;
+
+    var li = document.createElement("li");
+    li.className = "list-group-item";
+    li.appendChild(document.createTextNode(mergedText));
+    itemList.appendChild(li);
+
+    document.getElementById("username").value="";
+    document.getElementById("mobile").value="";
+    document.getElementById("email").value="";
+
+    localStorage.setItem(email,mergedText);
+    
+}
