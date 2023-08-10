53c76ccacc7dcd973ffc9d65878f55995ea219ba
saihyny <satyasaikiranrocks@gmail.com>
2023-08-10 21:50:29 +0530

    sharpner

--- html.js
+++ html.js
@@ -1,6 +1,12 @@
-var headerTitle= document.getElementById("main-header");
-var header = document.getElementById("header-title");
-headerTitle.textContent="hello"
-header.style.borderBottom = "solid 3px #000";
-var item= document.querySelector('#items li:nth-child(5)')
-item.style.backgroundColor="green"
+// var headerTitle= document.getElementById("main-header");
+// var header = document.getElementById("header-title");
+// headerTitle.textContent="hello"
+// header.style.borderBottom = "solid 3px #000";
+// var item= document.querySelector('#items li:nth-child(5)')
+// item.style.backgroundColor="green"
+var items = document.getElementsByClassName("list-group-item");
+items[2].style.backgroundColor="green";
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight="bold";
+}
